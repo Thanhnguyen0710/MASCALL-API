ff2ae35d3985277ab8b4592693ed6e1828b4f05d
Thanhnguyen0710 <56418634+Thanhnguyen0710@users.noreply.github.com>
2022-03-06 17:22:27 +0700

    update api get chat room

--- src/controllers/chatRoom.controller.js
+++ src/controllers/chatRoom.controller.js
@@ -20,16 +20,17 @@ module.exports.getRoom = async (req, res) => {
   const query = req.query.email;
   try {
     const chatRooms = await ChatRoom.find({email: new RegExp(query), lastMessage: {"$ne": null}}).sort({updatedDate: -1});
-    chatRooms.forEach(async data => {
-      if (data.email.length === 2) {
-        const emailFriend = data.email.filter(email => email !== query)[0];
+    for (let i = 0 ; i < chatRooms.length ; i++) {
+      if (chatRooms[i].email.length === 2) {
+        const emailFriend = chatRooms[i].email.filter(email => email !== query)[0];
         const userFriend = await User.findOne({email: emailFriend});
         if (userFriend) {
-          data.name = userFriend.displayName;
-          data.photoURL = userFriend.photoURL;
+          chatRooms[i].name = userFriend.displayName;
+          chatRooms[i].photoURL = userFriend.photoURL;
         }
       }
-    })
+    }
+    
     res.status(200).send({
       errorCode: '0',
       errorMessages: 'Success',
